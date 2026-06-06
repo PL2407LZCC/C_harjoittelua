d52787d7694f5420d74b67edc873a0c1c3b4e845
Lipsanen Petrus Ilmari <petrus.i.lipsanen@student.jyu.fi>
2026-06-06 17:03:34 +0300

    refactored tests to use fixture and a helper function

--- parent_child_sim/tests/test_device.py
+++ parent_child_sim/tests/test_device.py
@@ -1,17 +1,41 @@
 import subprocess
+import pytest
 
-def test_single_addition():
-    #path to build
-    executable_path = "parent_child_sim/build/device_sim.exe"
+#adding a fixture to have the compilation automated for all tests
+@pytest.fixture(scope="session")
+def compiled_device():
+    source_file = "parent_child_sim/main.c"
+    exe_path = "parent_child_sim/build/device_sim.exe"
 
-    input_to_device = "5\n1\n0\n"
+    print("\n compiling... \n")
+    compile_process = subprocess.run(["gcc", source_file, "-o", exe_path])
+    assert compile_process.returncode == 0, "Compilation fail"
 
+    yield exe_path
+
+def run_device(executable_path, input_bytes):
     result = subprocess.run(
         [executable_path],
-        input=input_to_device,
+        input=input_bytes,
         capture_output=True,
         text=True
     )
+    return result.stdout
 
-    print(result.stdout)
-    assert "End sum: 6" in result.stdout
+def test_single_addition(compiled_device):
+
+    input_to_device = "5\n1\n0\n"
+    result = run_device(compiled_device, input_to_device)
+    print(result)
+    assert "End sum: 6" in result
+
+
+def test_single_addition_with_faulty_input(compiled_device):
+
+    input_to_device = "h\n0\n"
+    result = run_device(compiled_device, input_to_device)
+    print(result)
+    assert "has to be integer, please continue" in result
+    assert "End sum: 0" in result
+
+
