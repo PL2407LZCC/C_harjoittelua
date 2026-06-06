f09c9b868d250712bed8833e53e7f4897167cd8e
Lipsanen Petrus Ilmari <petrus.i.lipsanen@student.jyu.fi>
2026-06-06 15:09:39 +0300

    added input sanitation

--- main.c
+++ main.c
@@ -5,7 +5,13 @@ int main() {
     int sum = 0;
     printf("Will add provided integer values, stopping at 0 and report the sum\n");
     while (variable != 0){
-        scanf("%d", &variable);
+        int scan_num = scanf("%d", &variable);
+        if (scan_num <= 0){
+            printf("has to be integer, please continue\n");
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF){}
+            continue;
+        }
         sum = sum + variable;
     }
     printf("End sum: %d", sum);
